e7ced6ff69efe1515345cd6e9b17d3d7af57a94c
rapzaDev <rapzadev@gmail.com>
2022-09-26 21:29:51 -0300

    Criaçao do projeto de desafio 01 e adicionando arquivo globals do typescript para poder usar modulos e arquivos css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './global.css';
+import styles from './App.module.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className={styles.Header}></header>
+    </div>
+  )
+}
+
+export default App
